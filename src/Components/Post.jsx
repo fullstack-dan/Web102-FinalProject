1907cdca4855f63e4a127c4d80facd9fd0f66a63
fullstack-dan <aouloudan@icloud.com>
2023-11-14 22:20:49 -0500

    fixed issue with post linking

--- src/Components/Post.jsx
+++ src/Components/Post.jsx
@@ -4,8 +4,28 @@ import { Link } from "react-router-dom";
 import "./Post.css";
 import supabase from "../client";
 
+import char_one_head from "../assets/char_one_head.png";
+import char_two_head from "../assets/char_two_head.png";
+import char_three_head from "../assets/char_three_head.png";
+import char_four_head from "../assets/char_four_head.png";
+import char_five_head from "../assets/char_five_head.png";
+import char_six_head from "../assets/char_six_head.png";
+
+const icons = {
+  one: char_one_head,
+  two: char_two_head,
+  three: char_three_head,
+  four: char_four_head,
+  five: char_five_head,
+  six: char_six_head,
+};
+
 function Post({ thisPost, feedPreview }) {
   const [post, setPost] = useState(thisPost);
+  const [author, setAuthor] = useState({
+    username: "",
+    icon: "",
+  });
   const [displayContent, setDisplayContent] = useState("");
   const [comments, setComments] = useState(null);
 
@@ -18,6 +38,18 @@ function Post({ thisPost, feedPreview }) {
       setComments(data);
     };
     getComments();
+
+    const getAuthor = async () => {
+      const { data } = await supabase
+        .from("users")
+        .select("*")
+        .filter("username", "eq", post.author);
+      setAuthor({
+        username: data[0].username,
+        icon: icons[data[0].avatar_id],
+      });
+    };
+    getAuthor();
   }, [post]);
 
   const getDate = (timestamp) => {
@@ -46,8 +78,10 @@ function Post({ thisPost, feedPreview }) {
     }
   }, [post, feedPreview]);
 
-  const handlePostUpvote = () => {
-    const updatePost = async () => {
+  const PostDiv = (post) => {
+    const [likes, setLikes] = useState(post.likes);
+
+    const handlePostUpvote = async () => {
       const { data, error } = await supabase
         .from("posts")
         .update({ likes: post.likes + 1 })
@@ -55,41 +89,13 @@ function Post({ thisPost, feedPreview }) {
       if (error) {
         console.log(error);
       }
-      setPost({ ...post, likes: post.likes + 1 });
-    };
-    updatePost();
-  };
-
-  const handleCommentUpvote = (e) => {
-    const commentId = e.currentTarget.getAttribute("data-comment-id");
-    const comment = comments.find((comment) => comment.id == commentId);
-
-    const updateComment = async () => {
-      const { data, error } = await supabase
-        .from("comments")
-        .update({ likes: comment.likes + 1 })
-        .eq("id", commentId);
-      if (error) {
-        console.log(error);
-      }
-      setComments(
-        comments.map((comment) => {
-          if (comment.id == commentId) {
-            return { ...comment, likes: comment.likes + 1 };
-          }
-          return comment;
-        })
-      );
+      setLikes(likes + 1);
     };
 
-    updateComment();
-  };
-
-  const PostDiv = (post) => {
     return (
       <div className="post">
         <div className="user-info">
-          <img src="/src/assets/char_one_head.png" alt="" />
+          <img src={author.icon} alt="" />
           <div className="user-text-info">
             <h3 className="user-name">{post.author}</h3>
             <p className="post-date">{getDate(post.created_at)}</p>
@@ -100,9 +106,62 @@ function Post({ thisPost, feedPreview }) {
           <p className="post-content">{displayContent}</p>
         </div>
         <div className="likes">
-          <span>{post.likes}</span>
+          <span>{likes}</span>
           <div className="upvote" onClick={handlePostUpvote}>
             ♡
+          </div>
+        </div>
+      </div>
+    );
+  };
+
+  const Comment = ({ comment }) => {
+    const [icon, setIcon] = useState("");
+    const [likes, setLikes] = useState(comment.likes);
+
+    useEffect(() => {
+      const getIcon = async (author) => {
+        const { data } = await supabase
+          .from("users")
+          .select("avatar_id")
+          .filter("username", "eq", author);
+
+        setIcon(icons[data[0].avatar_id]);
+      };
+      getIcon(comment.author);
+    }, [comment]);
+
+    const handleCommentUpvote = async () => {
+      const { data, error } = await supabase
+        .from("comments")
+        .update({ likes: comment.likes + 1 })
+        .eq("id", comment.id);
+
+      if (error) {
+        console.log(error);
+      } else {
+        setLikes(likes + 1);
+      }
+    };
+
+    return (
+      <div className="comments">
+        <div className="comment" key={comment.id}>
+          <div className="user-info">
+            <img src={icon} alt="" />
+            <div className="user-text-info">
+              <h3 className="user-name">{comment.author}</h3>
+              <p className="post-date">{getDate(comment.created_at)}</p>
+            </div>
+          </div>
+          <div className="post-text-info">
+            <p className="post-content">{comment.content}</p>
+          </div>
+          <div className="likes">
+            <span>{likes}</span>
+            <div className="upvote" onClick={handleCommentUpvote}>
+              ♡
+            </div>
           </div>
         </div>
       </div>
@@ -116,36 +175,13 @@ function Post({ thisPost, feedPreview }) {
       {feedPreview ? (
         <Link to={`/post/${post.id}`}>{PostDiv(post)}</Link>
       ) : (
-        PostDiv(post)
-      )}
-      {!feedPreview && (
         <>
+          {PostDiv(post)}
           <p className="add-comment">Add a comment +</p>
           <div className="comments">
             {comments &&
               comments.map((comment) => (
-                <div className="comment" key={comment.id}>
-                  <div className="user-info">
-                    <img src={comment.icon} alt="" />
-                    <div className="user-text-info">
-                      <h3 className="user-name">{comment.author}</h3>
-                      <p className="post-date">{getDate(comment.created_at)}</p>
-                    </div>
-                  </div>
-                  <div className="post-text-info">
-                    <p className="post-content">{comment.content}</p>
-                  </div>
-                  <div className="likes">
-                    <span>{comment.likes}</span>
-                    <div
-                      className="upvote"
-                      onClick={handleCommentUpvote}
-                      data-comment-id={comment.id}
-                    >
-                      ♡
-                    </div>
-                  </div>
-                </div>
+                <Comment comment={comment} key={comment.id} />
               ))}
           </div>
         </>
